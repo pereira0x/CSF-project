1112e16983175a60c18e45a85b010a67c0898a4a
pereira0x <jose.a.pereira03@gmail.com>
2024-10-24 12:06:50 +0100

    lab3: chatgpt and utils final

--- lab3/scripts/utils.py
+++ lab3/scripts/utils.py
@@ -3,9 +3,9 @@ import urllib.parse
 import datetime
 
 IPs = {
-        "194.210.61.135": "Rafael Calhau",
         "194.210.61.134": "Virgolino Gonçalves",
-        "94.210.61.136": "Miguel Estrela",
+        "194.210.61.135": "Rafael Calhau",
+        "194.210.61.136": "Nuno Santos",
         "194.210.63.254": "Router",
         "10.0.2.15": "Router (Public IP)",
 }
@@ -18,7 +18,7 @@ def resolve_host(host):
         if host == name:
             return ip
 
-def run_tshark(filters, fields, extra_flags = "", filename = "trace.pcapng"):
+def run_tshark(filters, fields, extra_flags = "", filename = "traces.pcapng"):
     def escape_filter(f):
         return f
     cmd = ["tshark", "-r", filename, "-T", "fields", "-Y '", escape_filter(filters), "'"]
@@ -39,6 +39,6 @@ def extract_urlencoded_parameter(url, parameter):
 def convert_time(time):
     # convert epoch time
     date = datetime.datetime.fromtimestamp(float(time))
-    date = date - datetime.timedelta(weeks=2)
+    date = date - datetime.timedelta(weeks=4.71428571)
     date = date.strftime("%Y-%m-%d %H:%M:%S")
     return date
